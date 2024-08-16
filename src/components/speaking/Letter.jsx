14c517c2a5ea736d7035a48dd5910e6d3da499e5
Sufferal <botnari_ciprian@mail.ru>
2024-08-16 11:28:01 +0300

    feat: add audio for whole alphabet

--- src/components/speaking/Letter.jsx
+++ src/components/speaking/Letter.jsx
@@ -1,8 +1,9 @@
 import "../../assets/scss/speaking/Letter.scss";
+import VolumeIcon from "../icons/VolumeIcon";
 
 const Letter = ({ objKey, objValue }) => {
   const audioHandler = (e) => {
-    let audioPath = e.target.value;
+    let audioPath = e.target.closest("button").value;
     let letterAudio = new Audio(audioPath);
     letterAudio.play();
   };
@@ -10,21 +11,26 @@ const Letter = ({ objKey, objValue }) => {
   return (
     <div className="letter-wrapper">
       <h3 className="letter-name">{objKey}</h3>
+      {objValue.length === 0 ? (
+        <button className="letter-btn letter-btn-disabled">Silent</button>
+      ) : (
+        ""
+      )}
       {objValue.length === 1 ? (
         <button className="letter-btn" onClick={audioHandler} value={objValue}>
-          Play
+          <VolumeIcon />
         </button>
       ) : (
         <div className="letter-btn-wrapper">
           {objValue.map((audioValue, index) => (
-          <button
-            key={index}
-            className="letter-btn"
-            onClick={audioHandler}
-            value={audioValue}
-          >
-            Play
-          </button>
+            <button
+              key={index}
+              className="letter-btn"
+              onClick={audioHandler}
+              value={audioValue}
+            >
+              <VolumeIcon />
+            </button>
           ))}
         </div>
       )}
